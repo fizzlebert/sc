073f14c3965feccf2c9c7df129b3d86ddd46a6b1
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2018-12-30 11:08:39 +1100

    Update to use __version__ and update requirements

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
+from sc.__version__ import __author__, __author_email__, __title__, __version__
+
 import setuptools
 
 setuptools.setup(
-    name="sc",
-    version="0.0.1",
-    author="Daniel O'Connell",
-    author_email="dan.ben.oconnell@gmail.com",
+    name=__title__,
+    version=__version__,
+    author=__author__,
+    author_email=__author_email__,
     description="Download songs from SoundCloud.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/danieloconell/sc",
-    packages=setuptools.find_packages(),
-    install_requires=open("requirements.txt").read().split("\n"),
+    packages=["sc"],
+    install_requires=["mutagen", "requests", "docopt", "tqdm"],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
